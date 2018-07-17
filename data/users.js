f8f086f8bba5b8a6b8bfd3d3f06c31444c420778
myCallthomas <mycallthomas@gmail.com>
2018-07-17 16:39:34 +0100

    updated server with soundcloud and favourites

--- data/users.js
+++ data/users.js
@@ -1,153 +1,182 @@
 module.exports = [
-    {
-      id: 1,
-      name: "Ollie",
-      surname: "Crook",
-      image: " ",
-      locationIds: [1, 3],
-      instruments: ["Bass", "Guitar"],
-      genres: ["Jazz"],
-      social: {
-        soundcloud: "139581262",
-        twitter: "oilyquack",
-        instagram: "oilyquack"
-      },
-      availability: "weekends"
+  {
+    id: 1,
+    name: "Ollie",
+    surname: "Crook",
+    image: " ",
+    locationIds: [1, 3],
+    instruments: ["Bass", "Guitar"],
+    genres: ["Jazz"],
+    social: {
+      soundcloudProfile: "",
+      soundcloudTrack: "",
+      soundcloud: "139581262",
+      twitter: "oilyquack",
+      instagram: "oilyquack"
     },
-    {
-      id: 2,
-      name: "Ahmed",
-      surname: "ices",
-      image: " ",
-      location: [1, 2],
-      instruments: ["Drums", "Vocals","Piano"],
-      genres: ["Screams"],
-      social: {
-        soundcloud: " ",
-        twitter: "rices",
-        instagram: "licence",
-      },
-      availability: "everyDay"
+    availability: "weekends",
+    favourites: [2, 3]
+  },
+  {
+    id: 2,
+    name: "Ahmed",
+    surname: "ices",
+    image: " ",
+    location: [1, 2],
+    instruments: ["Drums", "Vocals", "Piano"],
+    genres: ["Screams"],
+    social: {
+      soundcloudProfile: "",
+      soundcloudTrack: "",
+      soundcloud: " ",
+      twitter: "rices",
+      instagram: "licence"
     },
-    {
-      id: 3,
-      name: "Michael",
-      surname: "Thomas",
-      image: " ",
-      location: [1, 3],
-      instruments: ["Drums", "Vocals"],
-      genres: ["Screams"],
-      social: {
-        soundcloud: " ",
-        twitter: "MycallThomas",
-        instagram: "MycallThomas"
-      },
-      availability: "Tuesday"
+    availability: "everyDay",
+    favourites: [4, 3]
+  },
+  {
+    id: 3,
+    name: "Michael",
+    surname: "Thomas",
+    image: " ",
+    location: [1, 3],
+    instruments: ["Drums", "Vocals"],
+    genres: ["Screams"],
+    social: {
+      soundcloudProfile: "",
+      soundcloudTrack: "",
+      soundcloud: " ",
+      twitter: "MycallThomas",
+      instagram: "MycallThomas"
     },
-    {
-      id: 4,
-      name: "Rafal",
-      surname: "Porzucek",
-      image: " ",
-      location: "London",
-      instruments: ["Bass", "Vocals"],
-      genres: ["Screams"],
-      social: {
-        soundcloud: " ",
-        twitter: "Rafal",
-        instagram: "Rafal"
-      },
-      availability: "Saturday"
+    availability: "Tuesday",
+    favourites: [2, 4]
+  },
+  {
+    id: 4,
+    name: "Rafal",
+    surname: "Porzucek",
+    image: " ",
+    location: "London",
+    instruments: ["Bass", "Vocals"],
+    genres: ["Screams"],
+    social: {
+      soundcloudProfile: "",
+      soundcloudTrack: "",
+      soundcloud: " ",
+      twitter: "Rafal",
+      instagram: "Rafal"
     },
-    {
-      id: 5,
-      name: "Matt",
-      surname: "Garrity",
-      image: " ",
-      location: "London",
-      instruments: ["Guitar", "Vocals"],
-      genres: ["Screams"],
-      social: {
-        soundcloud: " ",
-        twitter: "MattGarrity",
-        instagram: "MattGarrity"
-      },
-      availability: "everyDay"
+    availability: "Saturday",
+    favourites: [2, 3]
+  },
+  {
+    id: 5,
+    name: "Matt",
+    surname: "Garrity",
+    image: " ",
+    location: "London",
+    instruments: ["Guitar", "Vocals"],
+    genres: ["Screams"],
+    social: {
+      soundcloudProfile: "",
+      soundcloudTrack: "",
+      soundcloud: " ",
+      twitter: "MattGarrity",
+      instagram: "MattGarrity"
     },
-    {
-      id: 6,
-      name: "Whitney",
-      surname: "Houston",
-      image: " ",
-      locationIds: [4, 1],
-      instruments: ["Vocals", "Guitar"],
-      genres: ["R n B"],
-      social: {
-        soundcloud: " ",
-        twitter: "WhitneyHouston",
-        instagram: "WhitneyHouston"
-      },
-      availability: "weekends"
+    availability: "everyDay",
+    favourites: [2, 3]
+  },
+  {
+    id: 6,
+    name: "Whitney",
+    surname: "Houston",
+    image: " ",
+    locationIds: [4, 1],
+    instruments: ["Vocals", "Guitar"],
+    genres: ["R n B"],
+    social: {
+      soundcloudProfile: "",
+      soundcloudTrack: "",
+      soundcloud: " ",
+      twitter: "WhitneyHouston",
+      instagram: "WhitneyHouston"
     },
-    {
-      id: 7,
-      name: "James",
-      surname: "Blunt",
-      image: " ",
-      locationIds: [1, 3],
-      instruments: ["Vocals", "Guitar"],
-      genres: ["Jazz"],
-      social: {
-        soundcloud: " ",
-        twitter: "JamesBLunt",
-        instagram: "JamesBLunt"
-      },
-      availability: "weekends"
+    availability: "weekends",
+    favourites: [2, 3]
+  },
+  {
+    id: 7,
+    name: "James",
+    surname: "Blunt",
+    image: " ",
+    locationIds: [1, 3],
+    instruments: ["Vocals", "Guitar"],
+    genres: ["Jazz"],
+    social: {
+      soundcloudProfile: "",
+      soundcloudTrack: "",
+      soundcloud: " ",
+      twitter: "JamesBLunt",
+      instagram: "JamesBLunt"
     },
-    {
-      id: 8,
-      name: "Joe",
-      surname: "Spunge",
-      image: " ",
-      locationIds: [1, 3],
-      instruments: ["Guitar"],
-      genres: ["Jazz"],
-      social: {
-        soundcloud: " ",
-        twitter: "JoeSpunge",
-        instagram: "JoeSpunge"
-      },
-      availability: "weekends"
+    availability: "weekends",
+    favourites: [2, 3]
+  },
+  {
+    id: 8,
+    name: "Joe",
+    surname: "Spunge",
+    image: " ",
+    locationIds: [1, 3],
+    instruments: ["Guitar"],
+    genres: ["Jazz"],
+    social: {
+      soundcloudProfile: "",
+      soundcloudTrack: "",
+      soundcloud: " ",
+      twitter: "JoeSpunge",
+      instagram: "JoeSpunge"
     },
-    {
-      id: 9,
-      name: "Harry",
-      surname: "Styles",
-      image: " ",
-      locationIds: [1, 3],
-      instruments: ["Vocals"],
-      genres: ["Jazz"],
-      social: {
-        soundcloud: " ",
-        twitter: "HarryStyles",
-        instagram: "HarryStyles"
-      },
-      availability: "everyday"
+    availability: "weekends",
+    favourites: [2, 3]
+  },
+  {
+    id: 9,
+    name: "Harry",
+    surname: "Styles",
+    image: " ",
+    locationIds: [1, 3],
+    instruments: ["Vocals"],
+    genres: ["Jazz"],
+    social: {
+      soundcloudProfile: "",
+      soundcloudTrack: "",
+      soundcloud: " ",
+      twitter: "HarryStyles",
+      instagram: "HarryStyles"
     },
-    {
-      id: 10,
-      name: "Marc ",
-      surname: "Anthony",
-      image: " ",
-      locationIds: [1, 5],
-      instruments: ["Vocals", "Guitar"],
-      genres: ["Love Songs"],
-      social: {
-        soundcloud: " ",
-        twitter: "MarcAnthony",
-        instagram: "MarcAnthony"
-      },
-      availability: "weekends"
-    }
-   ];
-  
+    availability: "everyday",
+    favourites: [2, 3]
+  },
+  {
+    id: 10,
+    name: "Marc ",
+    surname: "Anthony",
+    image: " ",
+    locationIds: [1, 5],
+    instruments: ["Vocals", "Guitar"],
+    genres: ["Love Songs"],
+    social: {
+      soundcloudProfile: "",
+      soundcloudTrack: "",
+      soundcloud: " ",
+      twitter: "MarcAnthony",
+      instagram: "MarcAnthony"
+    },
+    availability: "weekends",
+    favourites: [2, 3]
+  }
+];
