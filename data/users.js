3d646d8f611a3da8be44d860ea0ac46d0f41d9bb
Matt <matthewgarrity85@mac.com>
2018-07-17 14:29:17 +0100

    extracted data into modules

---
+++ data/users.js
@@ -0,0 +1,153 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Ollie",
+    surname: "Crook",
+    image: "https://...",
+    locationIds: [1, 3],
+    instruments: ["Bass", "Guitar"],
+    genres: ["Jazz"],
+    social: {
+      soundcloud: "https://...",
+      twitter: "oilyquack",
+      instagram: "oilyquack"
+    },
+    availability: "weekends"
+  },
+  {
+    id: 2,
+    name: "Ahmed",
+    surname: "ices",
+    image: "https://...",
+    location: [1, 2],
+    instruments: ["Drums", "Vocals", "Piano"],
+    genre: ["Screams"],
+    social: {
+      soundcloud: "https://...",
+      twitter: "rices",
+      instagram: "licence",
+
+      availability: "everyDay"
+    }
+  },
+  {
+    id: 3,
+    name: "Michael",
+    surname: "Thomas",
+    image: "https://...",
+    location: [1, 3],
+    instruments: ["Drums", "Vocals"],
+    genre: ["Screams"],
+    social: {
+      soundcloud: "https://...",
+      twitter: "MycallThomas",
+      instagram: "MycallThomas"
+    },
+    availability: "Tuesday"
+  },
+  {
+    id: 4,
+    name: "Rafal",
+    surname: "Porzucek",
+    image: "https://...",
+    location: "London",
+    instruments: ["Bass", "Vocals"],
+    genre: ["Screams"],
+    social: {
+      soundcloud: "https://...",
+      twitter: "Rafal",
+      instagram: "Rafal"
+    },
+    availability: "Saturday"
+  },
+  {
+    id: 5,
+    name: "Matt",
+    surname: "Garrity",
+    image: "https://...",
+    location: "London",
+    instruments: ["Guitar", "Vocals"],
+    genre: ["Screams"],
+    social: {
+      soundcloud: "https://...",
+      twitter: "MattGarrity",
+      instagram: "MattGarrity"
+    },
+    availability: "everyDay"
+  },
+  {
+    id: 6,
+    name: "Whitney",
+    surname: "Houston",
+    image: "https://...",
+    locationIds: [4, 1],
+    instruments: ["Vocals", "Guitar"],
+    genres: ["R n B"],
+    social: {
+      soundcloud: "https://...",
+      twitter: "WhitneyHouston",
+      instagram: "WhitneyHouston"
+    },
+    availability: "weekends"
+  },
+  {
+    id: 7,
+    name: "James",
+    surname: "Blunt",
+    image: "https://...",
+    locationIds: [1, 3],
+    instruments: ["Vocals", "Guitar"],
+    genres: ["Jazz"],
+    social: {
+      soundcloud: "https://...",
+      twitter: "JamesBLunt",
+      instagram: "JamesBLunt"
+    },
+    availability: "weekends"
+  },
+  {
+    id: 8,
+    name: "Joe",
+    surname: "Spunge",
+    image: "https://...",
+    locationIds: [1, 3],
+    instruments: ["Guitar"],
+    genres: ["Jazz"],
+    social: {
+      soundcloud: "https://...",
+      twitter: "JoeSpunge",
+      instagram: "JoeSpunge"
+    },
+    availability: "weekends"
+  },
+  {
+    id: 9,
+    name: "Harry",
+    surname: "Styles",
+    image: "https://...",
+    locationIds: [1, 3],
+    instruments: ["Vocals"],
+    genres: ["Jazz"],
+    social: {
+      soundcloud: "https://...",
+      twitter: "HarryStyles",
+      instagram: "HarryStyles"
+    },
+    availability: "everyday"
+  },
+  {
+    id: 10,
+    name: "Marc ",
+    surname: "Anthony",
+    image: "https://...",
+    locationIds: [1, 5],
+    instruments: ["Vocals", "Guitar"],
+    genres: ["Love Songs"],
+    social: {
+      soundcloud: "https://...",
+      twitter: "MarcAnthony",
+      instagram: "MarcAnthony"
+    },
+    availability: "weekends"
+  }
+];
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,32 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-function Header(){
-
-    return (
-        <header>
-          <h1> MUSIC </h1>
-        
-          <ul >
-          <li >
-              <Link to="/all">ALL</Link>
-            </li>
-            <li >
-              <Link to="/guitar">GUITAR</Link>
-            </li>
-            <li >
-              <Link to="/drums">DRUMS</Link>
-            </li>
-            <li >
-              <Link to="/vocals">VOCALS</Link>
-            </li>
-            <li >
-              <Link to="/other">OTHER</Link>
-            </li>
-            <li >
-              <Link to="/create">CREATE POST</Link>
-            </li>
-          </ul>
-        
-      </header>
-    );
-  
-
+function Header() {
+  return (
+    <header>
+      <h1> MUSIC </h1>
+      <ul>
+        <li>
+          <Link to="/all">ALL</Link>
+        </li>
+        <li>
+          <Link to="/guitar">GUITAR</Link>
+        </li>
+        <li>
+          <Link to="/drums">DRUMS</Link>
+        </li>
+        <li>
+          <Link to="/vocals">VOCALS</Link>
+        </li>
+        <li>
+          <Link to="/other">OTHER</Link>
+        </li>
+        <li>
+          <Link to="/create">CREATE POST</Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
-
 
 export default Header;
