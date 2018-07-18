253a7a038e696c8b0fb788a3b589f6f30669a285
Ahmed <dulaimi89@gmail.com>
2018-07-18 12:46:56 +0100

    wall post code start

---
+++ src/containers/PostContainer.js
@@ -0,0 +1,25 @@
+import { connect } from "react-redux";
+
+import Posts from '../components/Users';
+import { fetchUsers } from "../actions"
+
+const mapStateToProps = state => {
+
+    return {
+        musicians: state.users,
+        selected: state.select,
+        isLogged: state.isLogged
+    };
+
+}
+
+const mapDispatchToProps = dispatch => {
+    dispatch(fetchUsers());
+
+    return {
+        getMusicians : () => dispatch(fetchUsers()),
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Posts);
