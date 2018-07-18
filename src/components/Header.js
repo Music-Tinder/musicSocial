171a6c8d481e2f4e11ea3da6609d36aa44fc8670
Rafal <r.porzucek@gmail.com>
2018-07-18 20:53:58 +0100

    Added logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="header">
-      <img className="" src="../static/assets/logo.png" />
-      <h1> MUSIC </h1>
+      <img className="header__logo" src="../static/assets/logo.png" />
+      <h1>BANDIFY</h1>
       <div className="header__nav">
         <p>
           <Link to="/home">HOME</Link>
