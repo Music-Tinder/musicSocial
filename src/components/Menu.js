ba13356e7ac1176cac78e3be40f9c065a2e855ef
Ollie Crook <olliejcrook@gmail.com>
2018-07-18 12:42:50 +0100

    push header and footer to top and bottom

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,34 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import UsersContainer from '../containers/UsersContainer'
-import Others from '../components/Others'
+import UsersContainer from "../containers/UsersContainer";
+import Others from "../components/Others";
 import ProfileContainer from "../containers/ProfileContainer";
 
 class Menu extends React.Component {
-
-
-    render(){
-        return(
-            <Switch>
-
-            <Route path="/all" render={() => <UsersContainer /> }/>
-            <Route path="/guitar" render={() => <UsersContainer instrument="Guitar" /> }/>
-            <Route path="/drums" render={() => <UsersContainer instrument="Drums" /> }/>
-            <Route path="/vocals" render={() => <UsersContainer instrument="Vocals" /> }/>
-            <Route path="/other" component={Others} />
-            <Route path="/create"  />
-            <Route path="/profile" component={ProfileContainer}  />
-
-             </Switch>
-        )
-    }
-
-
+  render() {
+    return (
+      <div className="menu">
+        <Switch>
+          <Route path="/all" render={() => <UsersContainer />} />
+          <Route
+            path="/guitar"
+            render={() => <UsersContainer instrument="Guitar" />}
+          />
+          <Route
+            path="/drums"
+            render={() => <UsersContainer instrument="Drums" />}
+          />
+          <Route
+            path="/vocals"
+            render={() => <UsersContainer instrument="Vocals" />}
+          />
+          <Route path="/other" component={Others} />
+          <Route path="/create" />
+          <Route path="/profile" component={ProfileContainer} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-
-
 
 export default Menu;
