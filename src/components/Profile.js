5f9d1e8bbc1b3fe08dc27a686ccc9653106bb186
Ollie Crook <olliejcrook@gmail.com>
2018-07-19 14:10:39 +0100

    profiles page styled

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,46 +1,43 @@
 import React from "react";
-import EditProfile from '../components/EditProfile';
+import EditProfile from "../components/EditProfile";
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = { selectedPofile: {}, isSelected: false };
+    this.changeHandler = this.changeHandler.bind(this);
+  }
 
-    constructor(){
-        super();
-        this.state={selectedPofile:{}, isSelected:false }
-        this.changeHandler=this.changeHandler.bind(this)
-    }
+  changeHandler(event) {
+    let tempObj = this.props.musicians[event.target.value - 1];
+    this.props.selectMusician(tempObj);
+    this.props.logIn(true);
+    this.setState({ isSelected: true });
+  }
 
-    changeHandler(event){
-       
-        let tempObj= this.props.musicians[event.target.value-1]
-        this.props.selectMusician(tempObj);
-        this.props.logIn(true);
-        this.setState({isSelected:true})
+  render() {
+    return (
+      <div className="users">
+        <select onChange={this.changeHandler}>
+          {this.props.musicians.map(musician => {
+            return (
+              <option value={musician.id} key={musician.id}>
+                {" "}
+                {musician.name}{" "}
+              </option>
+            );
+          })}
+        </select>
 
-
-    }
-
-
-    render(){
-        
-        return(
-            <div>
-                <select onChange={this.changeHandler} >
-                    {this.props.musicians.map(musician=>{
-                        return(
-                        <option value={musician.id} key={musician.id}> {musician.name} </option>
-                        )
-                    })}
-
-                    </select>
-                   
-                    <EditProfile isSelected={this.props.isLogged} profile={this.props.selected}
-                     profiles={this.props.musicians} isLogged={this.props.isLogged} />
-
-              </div>
-        )
-    }
-
+        <EditProfile
+          isSelected={this.props.isLogged}
+          profile={this.props.selected}
+          profiles={this.props.musicians}
+          isLogged={this.props.isLogged}
+        />
+      </div>
+    );
+  }
 }
-
 
 export default Profile;
